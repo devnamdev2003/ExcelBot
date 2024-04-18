e21bf0ec99ff2e758fce0b2bb8683da2844e8f1b
Dev Namdev <devnamdevcse@gmail.com>
2024-04-18 14:33:02 +0530

    change db

--- api/views.py
+++ api/views.py
@@ -5,6 +5,11 @@ import os
 import json
 import concurrent.futures
 import time
+from dotenv import load_dotenv
+import google.generativeai as genai
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 # Move the OpenAI API key setup outside of the function for better performance.
 openai.api_key = os.getenv('OPENAI_KEY')
 
@@ -20,8 +25,10 @@ def user_input(request):
             data = json.loads(request.body)
 
             if 'csv_data' in data and 'user_input_message' in data:
+                lines = data['csv_data'].replace("\r", "").split("\n")
+                Ai_csv_data = "\n".join(lines[:15])
                 # Construct the conversation input by including the system message and user input.
-                user_message = "Tabel dataset: \n\n" + data['csv_data'].replace(
+                user_message = "Tabel dataset: \n\n" + Ai_csv_data.replace(
                     "\r", "") + '\nUser query: ' + data['user_input_message']
                 conversation = [
                     {"role": "system", "content": SYSTEM_MESSAGE},
@@ -34,7 +41,7 @@ def user_input(request):
                     ai_output = "Timed out please try again"
                     try:
                         ai_output = executor.submit(
-                            get_ai_response, conversation).result(timeout=40)
+                            get_ai_response_google, conversation).result(timeout=40)
                     except concurrent.futures.TimeoutError:
                         pass
                 response_data = {
@@ -71,24 +78,34 @@ def user_input(request):
         print("Invalid request method: Must be a POST request.")
         return JsonResponse(response_data, status=400)
 
-
-def get_ai_response(conversation):
-    print("Received a request to get AI response.")
+def get_ai_response_openai(conversation):
+    print("Received a request by openai to get AI response.")
     try:
-        # Use the provided conversation in the OpenAI API request.
         completion = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=conversation
         )
         response_text = completion.choices[0].message.content
-        # time.sleep(3)
+        # response_text = "hi how can i help you.."
+        # time.sleep(12)
         # response_text = "hi how can i help you.."
         print("AI response received.")
         return response_text
     except Exception as e:
-        # Handle OpenAI API errors and provide specific error message.
-        response_data = {
-            'error': f'OpenAI API error: {str(e)}'
-        }
         print(f"OpenAI API error: {str(e)}")
+        return get_ai_response_google(conversation)
+
+
+def get_ai_response_google(conversation):
+    print("Received a request by google to get AI response.")
+    try:
+        text = f"{conversation[1]['content']}\n{conversation[0]['content']}"
+        model = genai.GenerativeModel('gemini-pro')
+        response = model.generate_content(text)
+        response_text = response.text
+        print("AI response received.")
+        return response_text
+    except Exception as e:
+        response_data = f'error: API error: {str(e)}'
+        print(f"API error: {str(e)}")
         return response_data
